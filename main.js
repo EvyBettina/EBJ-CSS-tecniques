8e761f58b49ad2f1f7898b3611be9fc9ffb44b4b
EvyBettina <evybettina@hotmail.com>
2024-09-06 11:59:28 +0200

    added button

---
+++ main.js
@@ -0,0 +1,8 @@
+function toggletheme () {
+//logic for toggle the theme
+console.log("Theme button clicker")
+}
+
+//find the button that should trigger the function
+let themeButton = document.getElementById("toogle-theme")
+themeButton.addEventListener("click", toggleTheme)
